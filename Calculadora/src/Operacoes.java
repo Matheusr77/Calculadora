32f92f806a6970a35f74185afb50abd21ef81b0f
Matheus Ribeiro de Oliveira <matheusrib0707@gmail.com>
2025-05-20 15:18:33 -0300

    Atualização

---
+++ Calculadora/src/Operacoes.java
@@ -0,0 +1,56 @@
+public class Operacoes {
+
+
+    public double adicao(double a, double b){
+        return a + b;
+    }
+
+    public double subtracao(double a, double b){
+        return a - b;
+    }
+
+    public double multiplicacao(double a, double b){
+        return a * b;
+    }
+
+    public double divisao(double a, double b){
+        return a / b;
+    }
+
+    public double raiz(double a){
+        return Math.sqrt(a);
+    }
+
+    public double exponencial(double base, double expoente){
+        return Math.pow(base, expoente);
+    }
+
+    public double exponecialDois(double base){
+        return Math.pow(base, 2);
+    }
+
+    public double exponencialTres(double base){
+        return Math.pow(base, 3);
+    }
+
+    public double exponencialBaseDez(double expoente){
+        return Math.pow(10, expoente);
+    }
+
+    public int fatorial(int a){
+        int fatorial = 1;
+        for (int i = 1; i <= a; i++){
+            fatorial = fatorial * i;
+        };
+        return fatorial;
+    }
+
+    public double porcentagem(double a, double percentual){
+        double resultado = (a * percentual) / 100;
+        return a - resultado;
+    }
+
+    public double trocaSinal(double a){
+        return -a;
+    }
+}
--- Calculadora/src/Tela.java
+++ Calculadora/src/Tela.java
@@ -1,56 +1,66 @@
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.border.*;
+import java.awt.Font;
 
-public class Tela extends JFrame {
-    private JPanel painelPrincipal;
-    private JPanel painelCentro;
+public class Tela extends JFrame{
+    private JPanel painel;
+    private JPanel painelBotoes;
     private JTextArea visor;
     private JButton[] botoes;
+    private JScrollPane scrollVisor;
 
-    public Tela() {
-        painelPrincipal = new JPanel(new BorderLayout());
-        painelCentro = new JPanel(new GridLayout(5, 5, 5, 5));
-        visor = new JTextArea(2, 20);
+    public Tela(){
+        painel = new JPanel(new BorderLayout(10, 10));
+        painel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        visor = new JTextArea(2, 25);
+        scrollVisor = new JScrollPane(visor);
+        painelBotoes = new JPanel(new GridLayout(5, 5, 5, 5));
+
+        //Criação dos Botões
         botoes = new JButton[25];
-
-        String[] textosBotoes = {
+        String[] botoesTexto = {
                 "AC/C", "+/-", "%", "RAIZ Q", "/",
                 "7", "8", "9", "x^Y", "*",
                 "4", "5", "6", "x^2", "-",
                 "1", "2", "3", "x^3", "+",
-                "0", ".", "X!", "10^X", "="
-        };
+                "0", ".", "X!", "10^X", "="};
 
-        for (int i = 0; i < botoes.length; i++) {
-            botoes[i] = new JButton(textosBotoes[i]);
-            painelCentro.add(botoes[i]);
+
+        for (int i = 0; i < botoes.length; i++){
+            botoes[i] = new JButton(botoesTexto[i]);
+            botoes[i].setBackground(Color.lightGray);
+            botoes[i].setForeground(Color.black);
+            botoes[i].setFont(botoes[i].getFont().deriveFont(15f));
+            botoes[i].setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED, new Color(60, 60, 60), Color.DARK_GRAY));
+            painelBotoes.add(botoes[i]);
         }
+
     }
 
-    public void configurarJanela() {
-        setTitle("Calculadora");
-        setSize(600, 400);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setResizable(false);
 
-        configurarPainel();
-
-        add(painelPrincipal);
+    public void configurarJanela(){
         setVisible(true);
+        setTitle("Calculadora");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 400);
+        setResizable(false);
+        setLocation(600,500);
+        add(painel);
+        configurarPainel();
     }
+    public void configurarPainel(){
+        painel.setBackground(new Color(30, 30, 30));
 
-    private void configurarPainel() {
-        // Configurar o visor
-        visor.setFont(new Font("Arial", Font.BOLD, 24));
-        visor.setEditable(false);
-        visor.setBackground(Color.WHITE);
+        visor.setBackground(Color.white);
+        visor.setForeground(Color.black);
         visor.setLineWrap(true);
-        JScrollPane scrollVisor = new JScrollPane(visor);
+        visor.setWrapStyleWord(true);
+        visor.setEditable(false);
+        painelBotoes.setBackground(Color.darkGray);
 
-        // Adiciona visor ao norte e painelCentro ao centro
-        painelPrincipal.add(scrollVisor, BorderLayout.NORTH);
-        painelPrincipal.add(painelCentro, BorderLayout.CENTER);
+        painel.add(scrollVisor, BorderLayout.NORTH);
+        painel.add(painelBotoes, BorderLayout.CENTER);
+
     }
-
 }
